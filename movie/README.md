6ad8bcf6e7935f99afaa185dbd225d8c98a2af67
Kiranpjk <142809592+Kiranpjk@users.noreply.github.com>
2024-04-26 20:32:41 +0530

    Delete movie directory

--- movie/README.md
+++
@@ -1,1 +0,0 @@
-This is my react project of movie streaming website
--- movie/src/components/NavList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './navList.css';
-function NavList({ nav }) {
-  return (
-    <li><a href={nav.link}>{nav.name}</a></li>
-  )
-}
-
-export default NavList;
